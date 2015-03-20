6d67b1c1849a9471f4ca2d32b07842e3cbab7686
Sourav Singh <ssouravsingh12@gmail.com>
2015-03-20 09:06:00 -0700

    Update elevator.py

--- 01elevator_python/elevator.py
+++ 01elevator_python/elevator.py
@@ -3,9 +3,7 @@
 # * Python code for Traffic Signal Simulation using
 # * Beaglebone Black running Debian 7 Linux distribution
 ##########################################################
-# * Developed by MicroEmbedded Technologies
-##########################################################
- 
+
 
 import sys
 import time
