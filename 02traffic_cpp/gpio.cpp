8becc3efd8699def4e46b37c645c521e8d787443
Sourav Singh <ssouravsingh12@gmail.com>
2015-03-20 09:10:12 -0700

    Update gpio.cpp

--- 02traffic_cpp/gpio.cpp
+++ 02traffic_cpp/gpio.cpp
@@ -1,9 +1,7 @@
 /*******************************************************
  * C++ code for GPIO Functions for Traffic Signal Simulation
  * using Baglebone Black running Debian 7 Linux distribution
- ********************************************************
- * Developed by MicroEmbedded Technologies
- *******************************************************/
+ ********************************************************/
 
 #include<iostream>
 #include<fstream>
